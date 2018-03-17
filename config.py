027046e86c0e1119b0ad7278ba7ea9a301772e5f
Oliver <6378600+71Oliver@users.noreply.github.com>
2018-03-17 13:11:16 +0000

    Updated config.py for GOAT Support

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-token = ''
+discord_token = ''
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
 
-api_url = 'https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query'
+goat_url = 'https://2fwotdvm2o-dsn.algolia.net/1/indexes/ProductTemplateSearch/query'
+stockx_url = 'https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query'
