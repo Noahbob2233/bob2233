24d0cca2ada3cb64a51dd68573ddfe2e72c50643
Noahbob2233 <34804895+Noahbob2233@users.noreply.github.com>
2018-05-06 00:48:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-discord_token = ''
+discord_token = 'NDQyNTQ3OTY0MDI0NzE3MzEy.DdAacQ._iDLpBSxzKb8T5rZDh4De9A1sXc'
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
 
